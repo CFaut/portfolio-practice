c10a74f9f38080012c9a632ebcce61dae4c8759a
Casey Fautley <86919186+CFaut@users.noreply.github.com>
2022-02-11 17:32:29 +0200

    Project section interactivity added

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-let projectBlock = document.body.project.innerHTML;
+let project = document.getElementById("project-grid");
+console.log(project);
 
-function showProject(event) {
-   projectBlock.style.display = 'block'
+let showText = function (projects) {
+   
+    console.log('showtext is running')
+
+    for(let i = 0; i < projects.children.length; i++) {
+        console.log(projects[i]);
+        let child = projects.children[i];
+        let showDescription = function() {
+            child.children[1].classList.add('project-active');
+            child.children[0].classList.add("image-active");
+        }
+        
+      child.addEventListener('mouseenter', showDescription);
+    };
 }
 
-projectBlock.onclick = showProject;
+showText(project);
+
+let hideText = function (projects) {
+   
+    console.log('hidetext is running')
+
+    for(let i = 0; i < projects.children.length; i++) {
+        console.log(projects[i]);
+        let child = projects.children[i];
+        let hideDescription = function() {
+            child.children[1].classList.add('project-after');
+            child.children[0].classList.add("image-after");
+        }
+        
+      child.addEventListener('mouseleave', hideDescription);
+    };
+}
+ hideText(project);
